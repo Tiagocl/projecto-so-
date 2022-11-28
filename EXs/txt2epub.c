a4fa239094c0040c86d059f394432cec26edcf52
Tiagocl <up202004374@up.pt>
2022-11-28 14:41:54 +0000

    Q2 done

--- EXs/txt2epub.c
+++ EXs/txt2epub.c
@@ -13,20 +13,31 @@ int main(int argc, char* argv[]) {
 
         for (int i = 1; i < argc; i++) {
             if (fork() == 0) {
+
+                //str is .txt file
                 int length = strlen(argv[i]);
                 char *str = (char*)malloc(length);
                 strcpy(str,argv[i]);
+
+                //ptr is .epub file 
                 char *ptr = (char*)malloc(length+1);
                 char *x; 
                 x = strrchr(argv[i], '.');
                 strcpy(x,extension);
                 strcat(ptr,argv[i]);
+
                 printf ("[%d] converting %s...\n", getpid(),str);
 
-                fputs(ptr,stdout);
+                //converting
                 char buffer[40];
                 snprintf(buffer, sizeof(buffer), "pandoc %s -o %s", str, ptr);
                 system(buffer);
+
+                //creating zip and storing epub files 
+                char buf[40];
+                snprintf(buf, sizeof(buf), "zip ebooks.zip %s", ptr);
+                system(buf);
+                
 
                 free(str);
                 free(ptr);
